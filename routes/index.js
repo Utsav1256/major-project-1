25be19091981d41532dce1e1468d3a3c48e87a32
Utsav <utsavkumar5462@gmail.com>
2022-09-17 19:48:15 +0530

    create homeController.js in controllers folder, create an exported fn. in home controller.js require that fn. in router/index.js

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,9 @@ const router = express.Router();
 
 console.log('router loaded');
 
-module.exports = router; //1. here we have exorted thid router
+//2. to acces that fn.
+const homeController = require('../controllers/home_controller')
+
+router.get('/', homeController.home);
+
+module.exports = router; //1. here we have exorted the router
