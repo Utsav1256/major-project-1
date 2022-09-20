18053bb37a93009d82ed4c99dd9b69f4aa33f796
Utsav <utsavkumar5462@gmail.com>
2022-09-20 20:28:39 +0530

    create users_controller.js in controllers folder, created a fn. in it

---
+++ controllers/users_controller.js
@@ -0,0 +1,9 @@
+module.exports.profile = function(request, response) {
+    response.end('<h1>User Profile</h1>');
+}
+// Now this controller is ready to be access by a router, now that route needs to be access by my browser, browser tells me that you need to go to this route and the conntroller/action returns whatever data it has. 
+// if the controller/action is not present then it throws an error that, you cannot access it.
+// ok!
+// Now I need to create a route for it.
+// Everytime I create a controller/action, if I wanted to be accessible, it needs a route.
+// so i will go and access a route.
--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,14 @@ console.log('router loaded');
 const homeController = require('../controllers/home_controller')
 
 router.get('/', homeController.home);
+// thisrouter is accessing home_controller
+
+// considering this to be the route of routes (or index of routes), I want thisroute to be controlling all the other routes or having the list of all the other routes.
+router.use('/users', require('./users'));
+
+//for any further routes access from here
+// router.use('/routerName', require('./routerfile)); 
+
+
 
 module.exports = router; //1. here we have exorted the router
