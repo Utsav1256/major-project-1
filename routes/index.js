5ef564bcd12b0b55f7130982ecfd1f2e177a3753
Utsav <utsavkumar5462@gmail.com>
2022-10-21 01:10:17 +0530

    add postsController in users.js return HTML to the Browser

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.use('/users', require('./users'));
 //for any further routes access from here
 // router.use('/routerName', require('./routerfile)); 
 
-router.use('/posts', require('./posts'));
+
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,13 @@ const router = express.Router();//here we have defined the router
 //Now I need to access my controller (users_controller.js) from this route(users.js)
 const usersController = require('../controllers/users_controller');
 
-
-
-
+const postsController = require('../controllers/posts_controller');
 
 
 // now I just need to map a route to this users_controller's fn.
-router.get('/zumbo', usersController.profile);
+router.get('/profile', usersController.profile);
+
+router.get('/posts', postsController.posts);
 
 
 
