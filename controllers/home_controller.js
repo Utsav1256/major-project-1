b3362bfea2f4cc58b153b579c4d692e3b9b4c604
Utsav <utsavkumar5462@gmail.com>
2022-10-22 13:44:40 +0530

    create a view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,9 @@
 module.exports.home = function(request, response) {
-    return response.end('<h1> Express is up for Codeial!</h1>');
+    // return response.end('<h1> Express is up for Codeial!</h1>'); // I remove this line because it is sending dometing directly to the Browser
+
+    return response.render('home', {
+        title: "Home"
+    });
 }
 // module .exports.home = function(request, response) {s}
-//2. Now I need to access this fn. in routes
+// Now I need to access this fn. in routes
