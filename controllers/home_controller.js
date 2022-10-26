665817ec76c354d116115330167c62c26111e436
Utsav <utsavkumar5462@gmail.com>
2022-10-26 16:34:17 +0530

    Create and Alter a Cookie

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,8 @@
 module.exports.home = function(request, response) {
     // return response.end('<h1> Express is up for Codeial!</h1>'); // I remove this line because it is sending dometing directly to the Browser
 
+    console.log(request.cookies);
+    response.cookie('user_id', 25);  //changing the value
     return response.render('home', {
         title: "Home"
     }); 
